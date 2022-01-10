d0f7d4f7a308d322d909021ae66cb0c6a2eeb1bb
mingi <toyo30@naver.com>
2022-01-11 00:01:17 +0900

    detail-page component add, remove

---
+++ TextBook/static/detail-page-component.js
@@ -0,0 +1,51 @@
+let listAddNum = 0;
+let bookTitle = $('#bookTitle').text() + ' ' + $('#bookLevel').text();
+function addContent(){
+    let bookInfo = $(this).text();
+    listAddNum++;
+    let content = `
+    <div class="m-component-area">
+        <p class="m-component-title"><span class="point">${listAddNum}</span>. ${bookTitle}</p>
+        <p class="m-component-subtitle">${bookInfo}</p>
+        <div class="m-component-select">
+            <div class="m-component-amount">
+                <div class="amount-box">
+                    <div class="amount-check">
+                        <div class="sign sign-mius">
+                            <img src="../static/detail-page/m-mius-btn.png" class="minus">
+                        </div>
+                        <div class="amount-num">
+                            <input type="number"  oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" /
+                            class="amount-count" maxlength="2" min="1" value="1">
+                        </div>
+                        <div class="sign sign-plus">
+                            <img src="../static/detail-page/m-plus-btn.png" class="plus">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="m-component-price"><span class="muti-book-price">17,910</span>원</div>
+        </div>
+
+        <span class="remove-component"></span>
+    </div>`
+
+    $('.mobile-float .m-component-box').append(content);
+
+    $('.label').text('분권 구매');
+
+}
+
+function removeContent() {
+    $(this).parent().remove();
+    listAddNum = 0;
+
+    $('.m-component-title .point').each(function() {
+        listAddNum++;
+        $(this).val(listAddNum);
+    });
+}
+
+
+$(document).on('click', '.optionItem', addContent);
+$(document).on('click', '.remove-component', removeContent);
